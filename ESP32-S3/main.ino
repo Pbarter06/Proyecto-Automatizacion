2a9d2653459aea4598cd2861dfdcf796096c9a88
Paula Barona <pbarter@upv.edu.es>
2026-04-28 18:43:46 +0200

    inicializacion

---
+++ ESP32-S3/main.ino
@@ -0,0 +1,9 @@
+/****************************************************************************
+ * 
+ * @details    : Implementación con ESP32-S3 de un sistema de control mediante comunicación con MQTT
+ * @author     : Paula Barona, Finn Perea, Ainhoa López, Claudia Moreno
+ * 
+ **************************************************************************
+ */
+
+ 
