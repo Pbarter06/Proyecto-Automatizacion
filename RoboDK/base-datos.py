ea3180a5c5c37ec7fad94ac76d75a6bbc46ce652
Paula Barona <pbarter@upv.edu.es>
2026-05-07 01:02:40 +0200

    Añadido CAJA LLENA en .py

--- RoboDK/base-datos.py
+++ RoboDK/base-datos.py
@@ -17,9 +17,15 @@ cur = conn.cursor()
 
 sql = """INSERT INTO Azulejo (N_serie, Estado, ID_lote)
 VALUES (%s, %s, %s)"""
-datos = ('S1-001', 'Bueno', 'L-001')
+datos = ('S1-001', 'bueno', 'L-001')
 cur.execute(sql, datos)
 conn.commit()
 
 
 # CAJA LLENA 
+
+sql = """INSERT INTO Caja_llena (ID_lote, Tamano, Tipo, Codigo_Compra)
+VALUES (%s, %s, %s, %s)"""
+datos = ('L1-001', 100, 'bueno', 'P25-001')
+cur.execute(sql, datos)
+conn.commit()
